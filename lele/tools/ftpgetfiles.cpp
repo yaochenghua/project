ab68586f562f655cea3b7aa426460ca4b45bfa46
yaochenghua <3436384065@qq.com>
2023-08-06 22:07:21 +0800

    1

--- lele/tools/ftpgetfiles.cpp
+++ lele/tools/ftpgetfiles.cpp
@@ -1,10 +1,10 @@
 /*
- * @Description: 基于ftp协议的文件传输模块，从本地上传文件到服务器
- * @Version: v1.0
+ * @Description: 基于ftp协议的文件传输模块，从服务器下载文件到本地(增加了增量下载ftp服务器文件的功能)
+ * @Version: v5.0
  * @Autor: lele
- * @Date: 2023-08-05 21:51:38
+ * @Date: 2023-08-05 21:31:12
  * @LastEditors: lele
- * @LastEditTime: 2023-08-05 21:55:27
+ * @LastEditTime: 2023-08-05 21:31:21
  */
 
 
@@ -20,12 +20,14 @@ struct st_arg
   char password[31];       // 远程服务端ftp的密码。
   char remotepath[301];    // 远程服务端存放文件的目录。
   char localpath[301];     // 本地文件存放的目录。
-  char matchname[101];     // 待上传文件匹配的规则。
-  int  ptype;              // 上传后客户端文件的处理方式：1-什么也不做；2-删除；3-备份。
-  char localpathbak[301];  // 上传后客户端文件的备份目录。
-  char okfilename[301];    // 已上传成功文件名清单。
+  char matchname[101];     // 待下载文件匹配的规则。
+  char listfilename[301];  // 下载前列出服务端文件名的文件。
+  int  ptype;              // 下载后服务端文件的处理方式：1-什么也不做；2-删除；3-备份。
+  char remotepathbak[301]; // 下载后服务端文件的备份目录。
+  char okfilename[301];    // 已下载成功文件名清单。
+  bool checkmtime;         // 是否需要检查服务端文件的时间，true-需要，false-不需要，缺省为false。
   int  timeout;            // 进程心跳的超时时间。
-  char pname[51];          // 进程名，建议用"ftpputfiles_后缀"的方式。
+  char pname[51];          // 进程名，建议用"ftpgetfiles_后缀"的方式。
 } starg;
 
 // 文件信息的结构体。
@@ -35,10 +37,10 @@ struct st_fileinfo
   char mtime[21];       // 文件时间。
 };
 
-vector<struct st_fileinfo> vlistfile1;    // 已上传成功文件名的容器，从okfilename中加载。
-vector<struct st_fileinfo> vlistfile2;    // 上传前列出客户端文件名的容器，从nlist文件中加载。
-vector<struct st_fileinfo> vlistfile3;    // 本次不需要上传的文件的容器。
-vector<struct st_fileinfo> vlistfile4;    // 本次需要上传的文件的容器。
+vector<struct st_fileinfo> vlistfile1;    // 已下载成功文件名的容器，从okfilename中加载。
+vector<struct st_fileinfo> vlistfile2;    // 下载前列出服务端文件名的容器，从nlist文件中加载。
+vector<struct st_fileinfo> vlistfile3;    // 本次不需要下载的文件的容器。
+vector<struct st_fileinfo> vlistfile4;    // 本次需要下载的文件的容器。
 
 // 加载okfilename文件中的内容到容器vlistfile1中。
 bool LoadOKFile();
@@ -49,11 +51,11 @@ bool CompVector();
 // 把容器vlistfile3中的内容写入okfilename文件，覆盖之前的旧okfilename文件。
 bool WriteToOKFile();
 
-// 如果ptype==1，把上传成功的文件记录追加到okfilename文件中。
+// 如果ptype==1，把下载成功的文件记录追加到okfilename文件中。
 bool AppendToOKFile(struct st_fileinfo *stfileinfo);
 
-// 把localpath目录下的文件加载到vlistfile2容器中。
-bool LoadLocalFile();
+// 把ftp.nlist()方法获取到的list文件加载到容器vlistfile2中。
+bool LoadListFile();
 
 CLogFile logfile;
 
@@ -67,8 +69,8 @@ void _help();
 // 把xml解析到参数starg结构中。
 bool _xmltoarg(char *strxmlbuffer);
 
-// 上传文件功能的主函数。
-bool _ftpputfiles();
+// 下载文件功能的主函数。
+bool _ftpgetfiles();
 
 CPActive PActive;  // 进程心跳。
 
@@ -100,20 +102,34 @@ int main(int argc,char *argv[])
 
   // logfile.Write("ftp.login ok.\n");  // 正式运行后，可以注释这行代码。
 
-  _ftpputfiles();
+  _ftpgetfiles();
 
   ftp.logout();
 
   return 0;
 }
 
-// 上传文件功能的主函数。
-bool _ftpputfiles()
+// 下载文件功能的主函数。
+bool _ftpgetfiles()
 {
-  // 把localpath目录下的文件加载到vlistfile2容器中。
-  if (LoadLocalFile()==false)
+  // 进入ftp服务端存放文件的目录。
+  if (ftp.chdir(starg.remotepath)==false)
   {
-    logfile.Write("LoadLocalFile() failed.\n");  return false;
+    logfile.Write("ftp.chdir(%s) failed.\n",starg.remotepath); return false;
+  }
+
+  // 调用ftp.nlist()方法列出服务端目录中的文件，结果存放到本地文件中。
+  if (ftp.nlist(".",starg.listfilename)==false)
+  {
+    logfile.Write("ftp.nlist(%s) failed.\n",starg.remotepath); return false;
+  }
+
+  PActive.UptATime();   // 更新进程的心跳。
+
+  // 把ftp.nlist()方法获取到的list文件加载到容器vlistfile2中。
+  if (LoadListFile()==false)
+  {
+    logfile.Write("LoadListFile() failed.\n");  return false;
   }
 
   PActive.UptATime();   // 更新进程的心跳。
@@ -143,10 +159,10 @@ bool _ftpputfiles()
     SNPRINTF(strremotefilename,sizeof(strremotefilename),300,"%s/%s",starg.remotepath,vlistfile2[ii].filename);
     SNPRINTF(strlocalfilename,sizeof(strlocalfilename),300,"%s/%s",starg.localpath,vlistfile2[ii].filename);
 
-    logfile.Write("put %s ...",strlocalfilename);
+    logfile.Write("get %s ...",strremotefilename);
 
-    // 调用ftp.put()方法把文件上传到服务端，第三个参数填true的目的是确保文件上传成功，对方不可抵赖。
-    if (ftp.put(strlocalfilename,strremotefilename,true)==false) 
+    // 调用ftp.get()方法从服务端下载文件。
+    if (ftp.get(strremotefilename,strlocalfilename)==false) 
     {
       logfile.WriteEx("failed.\n"); return false;
     }
@@ -155,26 +171,26 @@ bool _ftpputfiles()
 
     PActive.UptATime();   // 更新进程的心跳。
     
-    // 如果ptype==1，把上传成功的文件记录追加到okfilename文件中。
+    // 如果ptype==1，把下载成功的文件记录追加到okfilename文件中。
     if (starg.ptype==1) AppendToOKFile(&vlistfile2[ii]);
 
-    // 删除文件。
-    if (starg.ptype==2)
+    // 删除服务端的文件。
+    if (starg.ptype==2) 
     {
-      if (REMOVE(strlocalfilename)==false)
+      if (ftp.ftpdelete(strremotefilename)==false)
       {
-        logfile.Write("REMOVE(%s) failed.\n",strlocalfilename); return false;
+        logfile.Write("ftp.ftpdelete(%s) failed.\n",strremotefilename); return false;
       }
     }
 
-    // 转存到备份目录。
-    if (starg.ptype==3)
+    // 把服务端的文件转存到备份目录。
+    if (starg.ptype==3) 
     {
-      char strlocalfilenamebak[301];
-      SNPRINTF(strlocalfilenamebak,sizeof(strlocalfilenamebak),300,"%s/%s",starg.localpathbak,vlistfile2[ii].filename);
-      if (RENAME(strlocalfilename,strlocalfilenamebak)==false)
+      char strremotefilenamebak[301];
+      SNPRINTF(strremotefilenamebak,sizeof(strremotefilenamebak),300,"%s/%s",starg.remotepathbak,vlistfile2[ii].filename);
+      if (ftp.ftprename(strremotefilename,strremotefilenamebak)==false)
       {
-        logfile.Write("RENAME(%s,%s) failed.\n",strlocalfilename,strlocalfilenamebak); return false;
+        logfile.Write("ftp.ftprename(%s,%s) failed.\n",strremotefilename,strremotefilenamebak); return false;
       }
     }
   }
@@ -192,26 +208,28 @@ void EXIT(int sig)
 void _help()
 {
   printf("\n");
-  printf("Using:/project/tools1/bin/ftpputfiles logfilename xmlbuffer\n\n");
+  printf("Using:/project/tools1/bin/ftpgetfiles logfilename xmlbuffer\n\n");
 
-  printf("Sample:/project/tools1/bin/procctl 30 /project/tools1/bin/ftpputfiles /log/idc/ftpputfiles_surfdata.log \"<host>127.0.0.1:21</host><mode>1</mode><username>wucz</username><password>wuczpwd</password><localpath>/tmp/idc/surfdata</localpath><remotepath>/tmp/ftpputest</remotepath><matchname>SURF_ZH*.JSON</matchname><ptype>1</ptype><localpathbak>/tmp/idc/surfdatabak</localpathbak><okfilename>/idcdata/ftplist/ftpputfiles_surfdata.xml</okfilename><timeout>80</timeout><pname>ftpputfiles_surfdata</pname>\"\n\n\n");
+  printf("Sample:/project/tools1/bin/procctl 30 /project/tools1/bin/ftpgetfiles /log/idc/ftpgetfiles_surfdata.log \"<host>127.0.0.1:21</host><mode>1</mode><username>wucz</username><password>wuczpwd</password><localpath>/idcdata/surfdata</localpath><remotepath>/tmp/idc/surfdata</remotepath><matchname>SURF_ZH*.XML,SURF_ZH*.CSV</matchname><listfilename>/idcdata/ftplist/ftpgetfiles_surfdata.list</listfilename><ptype>1</ptype><remotepathbak>/tmp/idc/surfdatabak</remotepathbak><okfilename>/idcdata/ftplist/ftpgetfiles_surfdata.xml</okfilename><checkmtime>true</checkmtime><timeout>80</timeout><pname>ftpgetfiles_surfdata</pname>\"\n\n\n");
 
-  printf("本程序是通用的功能模块，用于把本地目录中的文件上传到远程的ftp服务器。\n");
+  printf("本程序是通用的功能模块，用于把远程ftp服务端的文件下载到本地目录。\n");
   printf("logfilename是本程序运行的日志文件。\n");
-  printf("xmlbuffer为文件上传的参数，如下：\n");
+  printf("xmlbuffer为文件下载的参数，如下：\n");
   printf("<host>127.0.0.1:21</host> 远程服务端的IP和端口。\n");
   printf("<mode>1</mode> 传输模式，1-被动模式，2-主动模式，缺省采用被动模式。\n");
   printf("<username>wucz</username> 远程服务端ftp的用户名。\n");
   printf("<password>wuczpwd</password> 远程服务端ftp的密码。\n");
-  printf("<remotepath>/tmp/ftpputest</remotepath> 远程服务端存放文件的目录。\n");
-  printf("<localpath>/tmp/idc/surfdata</localpath> 本地文件存放的目录。\n");
-  printf("<matchname>SURF_ZH*.JSON</matchname> 待上传文件匹配的规则。"\
-         "不匹配的文件不会被上传，本字段尽可能设置精确，不建议用*匹配全部的文件。\n");
-  printf("<ptype>1</ptype> 文件上传成功后，本地文件的处理方式：1-什么也不做；2-删除；3-备份，如果为3，还要指定备份的目录。\n");
-  printf("<localpathbak>/tmp/idc/surfdatabak</localpathbak> 文件上传成功后，本地文件的备份目录，此参数只有当ptype=3时才有效。\n");
-  printf("<okfilename>/idcdata/ftplist/ftpputfiles_surfdata.xml</okfilename> 已上传成功文件名清单，此参数只有当ptype=1时才有效。\n");
-  printf("<timeout>80</timeout> 上传文件超时时间，单位：秒，视文件大小和网络带宽而定。\n");
-  printf("<pname>ftpputfiles_surfdata</pname> 进程名，尽可能采用易懂的、与其它进程不同的名称，方便故障排查。\n\n\n");
+  printf("<remotepath>/tmp/idc/surfdata</remotepath> 远程服务端存放文件的目录。\n");
+  printf("<localpath>/idcdata/surfdata</localpath> 本地文件存放的目录。\n");
+  printf("<matchname>SURF_ZH*.XML,SURF_ZH*.CSV</matchname> 待下载文件匹配的规则。"\
+         "不匹配的文件不会被下载，本字段尽可能设置精确，不建议用*匹配全部的文件。\n");
+  printf("<listfilename>/idcdata/ftplist/ftpgetfiles_surfdata.list</listfilename> 下载前列出服务端文件名的文件。\n");
+  printf("<ptype>1</ptype> 文件下载成功后，远程服务端文件的处理方式：1-什么也不做；2-删除；3-备份，如果为3，还要指定备份的目录。\n");
+  printf("<remotepathbak>/tmp/idc/surfdatabak</remotepathbak> 文件下载成功后，服务端文件的备份目录，此参数只有当ptype=3时才有效。\n");
+  printf("<okfilename>/idcdata/ftplist/ftpgetfiles_surfdata.xml</okfilename> 已下载成功文件名清单，此参数只有当ptype=1时才有效。\n");
+  printf("<checkmtime>true</checkmtime> 是否需要检查服务端文件的时间，true-需要，false-不需要，此参数只有当ptype=1时才有效，缺省为false。\n");
+  printf("<timeout>80</timeout> 下载文件超时时间，单位：秒，视文件大小和网络带宽而定。\n");
+  printf("<pname>ftpgetfiles_surfdata</pname> 进程名，尽可能采用易懂的、与其它进程不同的名称，方便故障排查。\n\n\n");
 }
 
 // 把xml解析到参数starg结构中。
@@ -242,22 +260,29 @@ bool _xmltoarg(char *strxmlbuffer)
   if (strlen(starg.localpath)==0)
   { logfile.Write("localpath is null.\n");  return false; }
 
-  GetXMLBuffer(strxmlbuffer,"matchname",starg.matchname,100);   // 待上传文件匹配的规则。
+  GetXMLBuffer(strxmlbuffer,"matchname",starg.matchname,100);   // 待下载文件匹配的规则。
   if (strlen(starg.matchname)==0)
   { logfile.Write("matchname is null.\n");  return false; }
 
-  // 上传后客户端文件的处理方式：1-什么也不做；2-删除；3-备份。
+  GetXMLBuffer(strxmlbuffer,"listfilename",starg.listfilename,300);   // 下载前列出服务端文件名的文件。
+  if (strlen(starg.listfilename)==0)
+  { logfile.Write("listfilename is null.\n");  return false; }
+
+  // 下载后服务端文件的处理方式：1-什么也不做；2-删除；3-备份。
   GetXMLBuffer(strxmlbuffer,"ptype",&starg.ptype);   
   if ( (starg.ptype!=1) && (starg.ptype!=2) && (starg.ptype!=3) )
   { logfile.Write("ptype is error.\n"); return false; }
 
-  GetXMLBuffer(strxmlbuffer,"localpathbak",starg.localpathbak,300); // 上传后客户端文件的备份目录。
-  if ( (starg.ptype==3) && (strlen(starg.localpathbak)==0) )
-  { logfile.Write("localpathbak is null.\n");  return false; }
+  GetXMLBuffer(strxmlbuffer,"remotepathbak",starg.remotepathbak,300); // 下载后服务端文件的备份目录。
+  if ( (starg.ptype==3) && (strlen(starg.remotepathbak)==0) )
+  { logfile.Write("remotepathbak is null.\n");  return false; }
 
-  GetXMLBuffer(strxmlbuffer,"okfilename",starg.okfilename,300); // 已上传成功文件名清单。
+  GetXMLBuffer(strxmlbuffer,"okfilename",starg.okfilename,300); // 已下载成功文件名清单。
   if ( (starg.ptype==1) && (strlen(starg.okfilename)==0) )
   { logfile.Write("okfilename is null.\n");  return false; }
+
+  // 是否需要检查服务端文件的时间，true-需要，false-不需要，此参数只有当ptype=1时才有效，缺省为false。
+  GetXMLBuffer(strxmlbuffer,"checkmtime",&starg.checkmtime);
 
   GetXMLBuffer(strxmlbuffer,"timeout",&starg.timeout);   // 进程心跳的超时时间。
   if (starg.timeout==0) { logfile.Write("timeout is null.\n");  return false; }
@@ -268,21 +293,16 @@ bool _xmltoarg(char *strxmlbuffer)
   return true;
 }
 
-// 把localpath目录下的文件加载到vlistfile2容器中。
-bool LoadLocalFile()
+// 把ftp.nlist()方法获取到的list文件加载到容器vlistfile2中。
+bool LoadListFile()
 {
   vlistfile2.clear();
 
-  CDir Dir;
+  CFile  File;
 
-  Dir.SetDateFMT("yyyymmddhh24miss");
-
-  // 不包括子目录。
-  // 注意，如果本地目录下的总文件数超过10000，增量上传文件功能将有问题。
-  // 建议用deletefiles程序及时清理本地目录中的历史文件。
-  if (Dir.OpenDir(starg.localpath,starg.matchname)==false)
+  if (File.Open(starg.listfilename,"r")==false)
   {
-    logfile.Write("Dir.OpenDir(%s) 失败。\n",starg.localpath); return false;
+    logfile.Write("File.Open(%s) 失败。\n",starg.listfilename); return false;
   }
 
   struct st_fileinfo stfileinfo;
@@ -291,13 +311,28 @@ bool LoadLocalFile()
   {
     memset(&stfileinfo,0,sizeof(struct st_fileinfo));
    
-    if (Dir.ReadDir()==false) break;
+    if (File.Fgets(stfileinfo.filename,300,true)==false) break;
 
-    strcpy(stfileinfo.filename,Dir.m_FileName);   // 文件名，不包括目录名。
-    strcpy(stfileinfo.mtime,Dir.m_ModifyTime);    // 文件时间。
+    if (MatchStr(stfileinfo.filename,starg.matchname)==false) continue;
+
+    if ( (starg.ptype==1) && (starg.checkmtime==true) )
+    {
+      // 获取ftp服务端文件时间。
+      if (ftp.mtime(stfileinfo.filename)==false)
+      {
+        logfile.Write("ftp.mtime(%s) failed.\n",stfileinfo.filename); return false;
+      }
+    
+      strcpy(stfileinfo.mtime,ftp.m_mtime);
+    }
 
     vlistfile2.push_back(stfileinfo);
   }
+
+  /*
+  for (int ii=0;ii<vlistfile2.size();ii++)
+    logfile.Write("filename=%s=\n",vlistfile2[ii].filename);
+  */
 
   return true;
 }
@@ -309,7 +344,7 @@ bool LoadOKFile()
 
   CFile File;
 
-  // 注意：如果程序是第一次上传，okfilename是不存在的，并不是错误，所以也返回true。
+  // 注意：如果程序是第一次下载，okfilename是不存在的，并不是错误，所以也返回true。
   if ( (File.Open(starg.okfilename,"r"))==false )  return true;
 
   char strbuffer[501];
@@ -375,7 +410,7 @@ bool WriteToOKFile()
   return true;
 }
 
-// 如果ptype==1，把上传成功的文件记录追加到okfilename文件中。
+// 如果ptype==1，把下载成功的文件记录追加到okfilename文件中。
 bool AppendToOKFile(struct st_fileinfo *stfileinfo)
 {
   CFile File;
